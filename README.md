096022c61b60e975e9c548ef9d5343bbfe73279c
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2024-08-20 00:16:08 +0300

    change router

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
-# Стартовый шаблон React
+# Дипломная работа
 
 Включает в себя
 - react 18
 - typescript
 - storybook
-- jest
-- eslint
-- prettier
-- husky
+- ui Material
+- redux-toolkit
+- react hook form
+- rest API
+- react router
+- sass
 
 Проект включает в себя деплой на github pages и будет доступен по адресу
-https://your-account-name.github.io
 
-Для запуска нужно запушить любой коммит в main ветку.
-
-Настройку деплоя смотрите тут .github/workflows/main.yml
-
-Используйте [feature sliced design](https://feature-sliced.design/ru/docs/get-started/overview)
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter  } from 'react-router-dom';
 import App from './app/App';
 
 //process.env.PUBLIC_URL
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-        <BrowserRouter basename="">
+        <HashRouter basename="">
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
 );
